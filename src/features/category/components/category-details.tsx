6e740d07f81ad84706f3c081e7f5d4121bc435d2
minhhieple97 <hieplevuc@gmail.com>
2025-05-18 14:30:05 +0700

    feat: Create CategoryDetails component for managing category information with form handling and image upload

---
+++ src/features/category/components/category-details.tsx
@@ -0,0 +1,121 @@
+'use client';
+
+import { FC } from 'react';
+import { Category } from '@prisma/client';
+
+import { useCategoryForm } from '../hooks/use-category-form';
+
+import { AlertDialog } from '@/components/ui/alert-dialog';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  FormDescription,
+} from '@/components/ui/form';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { ImageUpload } from '@/components/shared/image-upload';
+
+type CategoryDetailsProps = {
+  data?: Category;
+};
+
+export const CategoryDetails: FC<CategoryDetailsProps> = ({ data }) => {
+  const { form, isLoading, handleSubmit, isEditing } = useCategoryForm(data);
+
+  return (
+    <AlertDialog>
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Category Information</CardTitle>
+          <CardDescription>
+            {isEditing
+              ? `Update ${data?.name} category information.`
+              : ' Lets create a category. You can edit category later from the categories table or the category page.'}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="image"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <ImageUpload
+                        type="profile"
+                        value={field.value.map((image) => image.url)}
+                        disabled={isLoading}
+                        onChange={(url) => field.onChange([{ url }])}
+                        onRemove={(url) =>
+                          field.onChange([...field.value.filter((current) => current.url !== url)])
+                        }
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Category name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                disabled={isLoading}
+                control={form.control}
+                name="url"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Category url</FormLabel>
+                    <FormControl>
+                      <Input placeholder="/category-url" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="featured"
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-start space-x-3 space-y-0 rounded-md border p-4">
+                    <FormControl>
+                      <Checkbox checked={field.value} onCheckedChange={field.onChange} />
+                    </FormControl>
+                    <div className="space-y-1 leading-none">
+                      <FormLabel>Featured</FormLabel>
+                      <FormDescription>This Category will appear on the home page</FormDescription>
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" disabled={isLoading}>
+                {isLoading
+                  ? 'Submitting...'
+                  : isEditing
+                  ? 'Save category information'
+                  : 'Create category'}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </AlertDialog>
+  );
+};
